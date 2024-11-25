f901c85e7cdb8ccdb06bc78e22d4c3b0886e127d
Veena Madhure <112148394+vna2004@users.noreply.github.com>
2024-11-25 17:25:00 +0530

    Update2 app.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,24 @@
+function displayPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 5,
+    cursor: "",
+  });
+}
+
 function generatePoem(event) {
   event.preventDefault();
 
-  new Typewriter("#poem", {
-    strings: `वो अपनों की बातें, वो अपनों की ख़ू—बू
-                <br>
-                हमारी ही हिंदी, हमारी ही उर्दू!`,
-    autoStart: true,
-    delay: 100,
-    cursor: "",
-  });
+  let instructionsInput = document.querySelector("#user-instructions");
+
+  let apiKey = "48d0033t9b99ac4ec498e3aeeof72239";
+  let context =
+    "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br /> tag. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with '~ Antika AI' inside a <strong> element at the end of the poem and NOT at the beginning. Do not include the html tag in final answer.";
+  let prompt = `User instructions: Generate a Hindi poem about ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
